d0194872ac82893465a0dc9f5abaa2d5ff1338c6
Cedric Noel <gimli@dnd.fr>
2018-07-27 17:02:02 +0200

    Ask questions to user

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const chalk  = require('chalk');
-const clear  = require('clear');
-const figlet = require('figlet');
-const files  = require('./lib/files');
+const chalk    = require('chalk');
+const clear    = require('clear');
+const figlet   = require('figlet');
+const files    = require('./lib/files');
+const inquirer = require('./lib/inquirer.js');
 
 clear();
 
 console.log(
-  chalk.yellow(
-    figlet.textSync('Wise Impact', { horizontalLayout: 'full' })
-  )
+	chalk.yellow(
+		figlet.textSync('Ginit', { horizontalLayout: 'full' })
+	)
 );
+
+
+if (files.directoryExists('.git')) {
+	console.log(chalk.red('Already a git repository!'));
+	process.exit();
+}
+
+/**
+ * run function
+ * 
+ * @return void
+ */
+const run = async() => {
+	// Run inquier file function
+	const credentials = await inquirer.askGithubCredentials();
+	// When the user answer we log the result
+  	console.log(credentials);
+}
+
+run();
